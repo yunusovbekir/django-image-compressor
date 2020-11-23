c0fb1ab9cd4475ab7492ca3b98f18c39768e7f00
Bakir Yunusov <bekir@selhome.az>
2020-11-23 13:18:26 +0400

    update

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ setup(
     name='django-image-compressor',
     version='0.1',
     description="Image compressor for Django apps.",
-    url='',
-    download_url='',
+    url='https://github.com/yunusovbekir/django-image-compressor',
+    download_url='https://github.com/yunusovbekir/django-image-compressor',
     author='Bakir Yunusov',
     author_email='yunusovbekir@gmail.com',
-    license='',
+    license='MIT License',
     packages=find_packages(),
     install_requires=PACKAGES,
     zip_safe=False,
-    keywords=['django', 'image', 'compressor']
+    keywords=['django', 'image', 'compressor'],
 )
