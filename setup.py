e3a47da2ebc63bcc126496079e84e7cfa1fe8632
Bakir Yunusov <bekir@selhome.az>
2020-11-23 13:51:42 +0400

    update

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
 PACKAGES = (
     'Django>=3.0',
     'Pillow>=8.0',
@@ -10,6 +16,7 @@ setup(
     name='django-image-compressor',
     version='0.1',
     description="Image compressor for Django apps.",
+    long_description=long_description,
     url='https://github.com/yunusovbekir/django-image-compressor',
     download_url='https://github.com/yunusovbekir/django-image-compressor',
     author='Bakir Yunusov',
