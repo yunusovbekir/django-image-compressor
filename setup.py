0ec5a56cc757e145acf9cea9313b2f36a46b3983
Bakir Yunusov <bekir@selhome.az>
2020-11-23 13:47:14 +0400

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 PACKAGES = (
-    'Django>=3.1',
+    'Django>=3.0',
     'Pillow>=8.0',
 )
 
@@ -19,18 +19,4 @@ setup(
     install_requires=PACKAGES,
     zip_safe=False,
     keywords=['django', 'image', 'compressor'],
-    classifiers=[
-        "Environment:: Web Environment",
-        "Framework:: Django",
-        "Framework:: Django:: 3.1",
-        "Intended Audience:: Developers",
-        "License:: OSI Approved:: MIT License",
-        "License Operating System:: OS Independent",
-        "Programming Language:: Python",
-        "Programming Language:: Python:: 3",
-        "Programming Language:: Python:: 3:: Only",
-        "Programming Language:: Python:: 3.6",
-        "Programming Language:: Python:: 3.7",
-        "Programming Language:: Python:: 3.8",
-    ]
 )
